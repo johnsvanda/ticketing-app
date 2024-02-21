5849492e68f8bede75670839c3aacaa3dfc586b8
Jan Švanda <svanda@goout.cz>
2024-02-21 09:27:30 +0100

    Add express-validator and routes for signin, signout, signup, and current user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,17 @@
 import express, { json } from 'express';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there !!');
-})
-
-app.get('/', (req, res) => {
-  res.send('Hi there !');
-})
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log('Listening on port 3000!!!');
 });
